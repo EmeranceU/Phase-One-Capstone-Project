93743f3291f5f44bd108e03eb3011a0cab420d07
EMe-U <e.umurerwa@alustudent.com>
2026-05-27 07:53:10 +0200

    Create GenericDAO interface

---
+++ src/main/java/com/app/igirepay/lab2/dao/GenericDAO.java
@@ -0,0 +1,16 @@
+package com.app.igirepay.lab2.dao;
+
+import java.util.List;
+
+public interface GenericDAO<T, ID> {
+
+    void save(T entity);
+
+    T findById(ID id);
+
+    List<T> findAll();
+
+    void update(T entity);
+
+    void delete(ID id);
+}
